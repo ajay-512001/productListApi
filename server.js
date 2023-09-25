06fd8240f8d5ded3e3f5d472006b68494e122fc9
ajay-512001 <ajaygajjarkar512001@gmail.com>
2023-09-25 23:19:17 +0530

    last change

--- server.js
+++ server.js
@@ -1,123 +1,46 @@
+require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
-const Product = require('./models/productModel')
-const app = express()
-const cors = require('cors');
+const proudctRoute = require('./routes/productRoute');
+const errorMiddleware = require('./middleware/errorMiddleware')
+var cors = require('cors')
 
-const corsOptions ={
-    origin:'http://192.168.133.9:4000/', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
+const app = express()
+
+const PORT = process.env.PORT || 3000
+const MONGO_URL = process.env.MONGO_URL
+const FRONTEND = process.env.FRONTEND
+
+var corsOptions = {
+    origin: FRONTEND,
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 
-app.use(cors(corsOptions));
+app.use(cors(corsOptions))
 app.use(express.json())
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" )
-    next();
-  });
 app.use(express.urlencoded({extended: false}))
 
 //routes
+
+app.use('/api/products', proudctRoute);
 
 app.get('/', (req, res) => {
     res.send('Hello NODE API')
 })
 
 app.get('/blog', (req, res) => {
-    res.send('Hello Blog, My name is Devadmin')
-})
-//get all
-app.get('/products' ,async (req, res) => {
-    try {
-        const products = await Product.find({});
-        res.status(200).json(products);
-    } catch (error) {
-        res.status(500).json({message: error.message})
-    }
-})
-// get one
-app.get('/products/:id',ajaycal, async(req, res) =>{
-    try {
-        // const {id} = req.params;
-        // const product = await Product.findById(id);
-        res.status(200).json(res.Product);
-    } catch (error) {
-        res.status(500).json({message: 'error2'})
-    }
+    res.send('Hello Blog, My name is Devtamin')
 })
 
-// add one
-app.post('/products', async(req, res) => {
-    try {
-        const product = await Product.create(req.body)
-        res.status(200).json(product);
-        
-    } catch (error) {
-        console.log(error.message);
-        res.status(500).json({message: error.message})
-    }
-})
-
-// update a product
-app.put('/products/:id', async(req, res) => {
-    try {
-        const {id} = req.params;
-        const product = await Product.findByIdAndUpdate(id, req.body);
-        // we cannot find any product in database
-        if(!product){
-            return res.status(404).json({message: `cannot find any product with ID ${id}`})
-        }
-        const updatedProduct = await Product.findById(id);
-        res.status(200).json(updatedProduct);
-        
-    } catch (error) {
-        res.status(500).json({message: error.message})
-    }
-})
-
-// delete a product
-
-app.delete('/products/:id', async(req, res) =>{
-    try {
-        const {id} = req.params;
-        const product = await Product.findByIdAndDelete(id);
-        if(!product){
-            return res.status(404).json({message: `cannot find any product with ID ${id}`})
-        }
-        res.status(200).json(product);
-        
-    } catch (error) {
-        res.status(500).json({message: error.message})
-    }
-})
-
-
-async function ajaycal(req, res, next) {
-    let product_id
-    try {
-        product_id = await Product.findById(req.params.id)
-        product_id["price"] = product_id["quantity"]*product_id["price"]
-      if (product_id == null) {
-        return res.status(404).json({ message: 'Cannot find Product' })
-      }
-    } catch (err) {
-      return res.status(500).json({ message: 'hello error' })
-    }
-  
-    res.Product = product_id
-    next()
-  }
-
+app.use(errorMiddleware);
 
 mongoose.set("strictQuery", false)
 mongoose.
-connect('mongodb+srv://AjayAdmin:Ajay512001@restapidev.iylsgal.mongodb.net/Node-API?retryWrites=true&w=majority')
+connect(MONGO_URL)
 .then(() => {
     console.log('connected to MongoDB')
-    app.listen(3000, ()=> {
-        console.log(`Node API app is running on port 3000`)
+    app.listen(PORT, ()=> {
+        console.log(`Node API app is running on port ${PORT}`)
     });
 }).catch((error) => {
     console.log(error)
